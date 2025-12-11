c8c834e104efc732bc7ea4971f845aee8a3f16b3
Nahum <nahummejiad@gmail.com>
2025-12-11 13:53:31 -0600

    Added PrimeFlex and created the navbar component

---
+++ src/app/shared/components/navbar-component/navbar-component.ts
@@ -0,0 +1,35 @@
+// navbar-component.ts
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Toolbar } from 'primeng/toolbar';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar-component',
+  imports: [
+    Toolbar,
+    ButtonModule,
+    InputTextModule,
+    IconField,
+    InputIcon,
+    DrawerModule,
+    RouterLink,
+    CommonModule,
+  ],
+  templateUrl: './navbar-component.html',
+  styleUrl: './navbar-component.scss',
+})
+export class NavbarComponent {
+  public visible: boolean = false;
+
+  menuItems = [
+    { label: 'Inicio', icon: 'pi pi-home', route: '/' },
+    { label: 'Buscar', icon: 'pi pi-search', route: '/search' },
+    { label: 'Populares', icon: 'pi pi-star', route: '/popular' },
+  ];
+}
