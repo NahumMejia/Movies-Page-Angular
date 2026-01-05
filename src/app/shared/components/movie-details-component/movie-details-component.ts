a1f8a1f95001569df677c2dd2c69bcc1e60a3c70
Nahum <nahummejiad@gmail.com>
2026-01-04 19:02:23 -0600

    Added: Movie Details page

--- src/app/shared/components/movie-details-component/movie-details-component.ts
+++ src/app/shared/components/movie-details-component/movie-details-component.ts
@@ -1,28 +1,30 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { Movie } from '../movie-card-component/interfaces/movie.interface';
-import { ActivatedRoute } from '@angular/router';
-import { NgOptimizedImage } from "@angular/common";
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AsyncPipe, DatePipe, NgOptimizedImage } from "@angular/common";
+import { Observable, switchMap } from 'rxjs';
+import { TagModule } from 'primeng/tag';
+import { Card } from "primeng/card";
+import { ChipModule } from 'primeng/chip';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-movie-details-component',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, AsyncPipe, TagModule, Card, ChipModule, DividerModule, DatePipe, ],
   templateUrl: './movie-details-component.html',
   styleUrl: './movie-details-component.scss',
 })
 export class MovieDetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
-  public readonly moviesService = inject(MoviesService);
-  public movie?: Movie;
+  private moviesService = inject(MoviesService);
 
+  movie$!: Observable<Movie>;
 
-  //TO DO: IMPROVE LOGIC
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const id = Number(params['id']);
-      this.moviesService.getMovieDetails(id).subscribe(
-        movie => this.movie = movie
-      );
-    });
+    window.scrollTo(0, 0);
+    this.movie$ = this.route.paramMap.pipe(
+      switchMap((pm) => this.moviesService.getMovieDetails(Number(pm.get('id'))))
+    );
   }
 }
